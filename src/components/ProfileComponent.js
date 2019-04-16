cea2c2edef29ffc95f985b4f20a69d67652c8f6d
mustafayaylali <b140910051@sakarya.edu.tr>
2019-04-16 17:34:45 +0300

    profil sayfası gorsel guncelleme

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -8,10 +8,13 @@ import {
   Image,
   Alert,
   ScrollView,
+  ImageBackground,
   TouchableHighlight,
   BackHandler
 } from 'react-native';
 
+import bgImage from '../../images/background_profile.jpg'
+import dersler from '../../images/dersler.png'
 
 export default class ProfileComponent extends Component {
 
@@ -35,20 +38,29 @@ export default class ProfileComponent extends Component {
     const userInfo = params ? params.userInfo : null;
 
     return (
-      <ScrollView style={styles.scrollContainer}>
+      <ImageBackground source={bgImage} style={styles.scrollContainer}>
 
         <View style={styles.container}>
           <View style={styles.box}>
             <Image style={styles.profileImage} source={{ uri: userInfo.avatar_url }} />
             <Text style={styles.name}>{userInfo.login} - {userInfo.type}</Text>
           </View>
+
           <View style={styles.buttonContainer}>
+
             <TouchableHighlight style={[styles.button, styles.buttonMessage]} onPress={() => this.onClickListener('message')}>
-              <Image style={styles.icon} source={{ uri: '../../images/dersler.png' }} />
+              <View style={{ flexDirection: "row" }}>
+                <Image style={styles.icon} source={dersler} />
+                <Text style={styles.btnText}> DERSLERİM</Text>
+              </View>
             </TouchableHighlight>
 
+
             <TouchableHighlight style={[styles.button, styles.buttonLike]} onPress={() => this.onClickListener('like')}>
-              <Image style={styles.icon} source={{ uri: 'https://img.icons8.com/color/48/000000/facebook-like.png' }} />
+              <View style={{ flexDirection: "row" }}>
+                <Image style={styles.icon} source={dersler} />
+                <Text>Abnc</Text>
+              </View>
             </TouchableHighlight>
 
             <TouchableHighlight style={[styles.button, styles.buttonLove]} onPress={() => this.onClickListener('love')}>
@@ -61,7 +73,7 @@ export default class ProfileComponent extends Component {
           </View>
         </View>
 
-      </ScrollView>
+      </ImageBackground>
     );
   }
 }
@@ -75,11 +87,13 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   box: {
-    marginTop: 10,
-    borderRadius:70,
-    backgroundColor: 'rgba(52, 52, 52, 0.1)',
+    width: '60%',
+    justifyContent: 'center',
     alignItems: 'center',
-    shadowColor: 'black',
+    marginLeft: '20%',
+    borderRadius: 5,
+    backgroundColor: 'rgba(178, 255, 249,0)',
+    shadowColor: 'white',
     shadowOpacity: .2,
     shadowOffset: {
       height: 1,
@@ -95,25 +109,23 @@ const styles = StyleSheet.create({
     borderRadius: 200
   },
   name: {
-    fontSize: 25,
-    marginBottom: 20,
-    fontWeight: 'bold',
-    color: '#000000',
+    fontSize: 20,
+    marginBottom: 10,
+    color: '#ffffff',
   },
   buttonContainer: {
-    flexDirection: 'row',
+    flex: 1,
     marginTop: 20,
   },
 
   button: {
-    width: 60,
-    height: 60,
+    width: '100%',
+    height: 80,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
-    borderRadius: 30,
-    margin: 10,
+    borderRadius: 10,
     shadowColor: 'black',
     shadowOpacity: .8,
     shadowOffset: {
@@ -135,7 +147,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#40E0D0",
   },
   icon: {
-    width: 35,
-    height: 35,
+    width: 45,
+    height: 45,
+  },
+  btnText: {
+    color: '#ffffff',
+    fontSize: 25,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold'
   }
 });
