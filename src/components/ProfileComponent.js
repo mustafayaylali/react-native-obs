dba7aa8c8d97a952d7e774b9debaea5671214467
mustafayaylali <b140910051@sakarya.edu.tr>
2019-04-16 14:00:38 +0300

    guncellestirme

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -8,13 +8,25 @@ import {
   Image,
   Alert,
   ScrollView,
-  TouchableHighlight
+  TouchableHighlight,
+  BackHandler
 } from 'react-native';
 
+
 export default class ProfileComponent extends Component {
-  
+
+  static navigationOptions = {
+    header: null
+  }
+
+  componentDidMount() { //back button disable
+    BackHandler.addEventListener('hardwareBackPress', function () {
+      return true;
+    });
+  }
+
   onClickListener = (viewId) => {
-    Alert.alert("Alert", "Button pressed "+viewId);
+    Alert.alert("Alert", "Button pressed " + viewId);
   }
 
   render() {
@@ -24,27 +36,27 @@ export default class ProfileComponent extends Component {
 
     return (
       <ScrollView style={styles.scrollContainer}>
-      
-      <View style={styles.container}>
+
+        <View style={styles.container}>
           <View style={styles.box}>
-            <Image style={styles.profileImage} source={{uri: userInfo.avatar_url}}/>
+            <Image style={styles.profileImage} source={{ uri: userInfo.avatar_url }} />
             <Text style={styles.name}>{userInfo.login} - {userInfo.type}</Text>
           </View>
           <View style={styles.buttonContainer}>
             <TouchableHighlight style={[styles.button, styles.buttonMessage]} onPress={() => this.onClickListener('message')}>
-              <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/48/000000/facebook-like.png'}}/>
+              <Image style={styles.icon} source={{ uri: 'https://img.icons8.com/color/48/000000/facebook-like.png' }} />
             </TouchableHighlight>
 
             <TouchableHighlight style={[styles.button, styles.buttonLike]} onPress={() => this.onClickListener('like')}>
-              <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/48/000000/facebook-like.png'}}/>
+              <Image style={styles.icon} source={{ uri: 'https://img.icons8.com/color/48/000000/facebook-like.png' }} />
             </TouchableHighlight>
 
             <TouchableHighlight style={[styles.button, styles.buttonLove]} onPress={() => this.onClickListener('love')}>
-              <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/48/000000/facebook-like.png'}}/>
+              <Image style={styles.icon} source={{ uri: 'https://img.icons8.com/color/48/000000/facebook-like.png' }} />
             </TouchableHighlight>
 
             <TouchableHighlight style={[styles.button, styles.buttonCall]} onPress={() => this.onClickListener('phone')}>
-              <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/48/000000/facebook-like.png'}}/>
+              <Image style={styles.icon} source={{ uri: 'https://img.icons8.com/color/48/000000/facebook-like.png' }} />
             </TouchableHighlight>
           </View>
         </View>
@@ -56,57 +68,58 @@ export default class ProfileComponent extends Component {
 const styles = StyleSheet.create({
 
 
-  scrollContainer:{
+  scrollContainer: {
     flex: 1,
   },
-  container:{
-    padding:20,
+  container: {
+    padding: 20,
   },
   box: {
-    marginTop:10,
+    marginTop: 10,
     backgroundColor: 'white',
     alignItems: 'center',
     shadowColor: 'black',
     shadowOpacity: .2,
     shadowOffset: {
-      height:1,
-      width:-2
+      height: 1,
+      width: -2
     },
-    elevation:2,
-    paddingTop:10
+    elevation: 2,
+    paddingTop: 10
   },
-  profileImage:{
-    width:300,
-    height:300,
-    marginBottom:20,
+  profileImage: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+    borderRadius: 200
   },
-  name:{
-    fontSize:35,
-    marginBottom:20,
+  name: {
+    fontSize: 25,
+    marginBottom: 20,
     fontWeight: 'bold',
     color: '#1E90FF',
   },
-  buttonContainer:{
-    flexDirection:'row',
-    marginTop:20,
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
   },
 
   button: {
-    width:60,
-    height:60,
+    width: 60,
+    height: 60,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:20,
-    borderRadius:30,
-    margin:10,
+    marginBottom: 20,
+    borderRadius: 30,
+    margin: 10,
     shadowColor: 'black',
     shadowOpacity: .8,
     shadowOffset: {
-      height:2,
-      width:-2
+      height: 2,
+      width: -2
     },
-    elevation:4,
+    elevation: 4,
   },
   buttonMessage: {
     backgroundColor: "#00BFFF",
@@ -121,7 +134,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#40E0D0",
   },
   icon: {
-    width:35,
-    height:35,
+    width: 35,
+    height: 35,
   }
 });
